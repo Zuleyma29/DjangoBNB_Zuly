fe7b6c1661273a34a369136c17f148b6352e8ea6
Zuleyma Pelcastre Vargas <pelcastrevargasz@gmail.com>
2025-03-18 10:49:38 -0600

    16 Get properties in the front end and list them

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,23 +1,30 @@
 import Image from "next/image"
+import  { PropertyType } from "./PropertyList";
 
-const PropertyListItem = () => {
+interface PropertyProps{
+    property: PropertyType
+}
+
+const PropertyListItem: React.FC <PropertyProps> = ({
+    property
+}) => {
     return(
         <div className="cursor-pointer">
             <div className=" relative overflow-hidden aspect-square rounded-xl">
                 <Image
                     fill
-                    src='/beach_1.jpg'
+                    src={property.image_url}
                     sizes="(max-width: 758px) 768px, (max-width:1200px):768, 768px"
                     className="hover:scale-110 object-cover transition h-full w-full"
                     alt="Beach house"
                     />
             </div>
             <div className="mt-2">
-                <p className="text-lg font-bold"> Property name</p>
+                <p className="text-lg font-bold"> {property.title}</p>
             </div>
 
             <div className="mt-2">
-                <p className="text-sm text-gray-500"><strong>$200</strong> dolares</p>
+                <p className="text-sm text-gray-500"><strong>${property.price_per_night}</strong> dolares</p>
             </div>
         </div>
     )
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import { hostname } from "os";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import ('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/**'
+      }
+    ]
+  }
+  
 };
 
 export default nextConfig;
