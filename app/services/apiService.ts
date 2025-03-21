c54422ddc8aa69cfbdd406c2cd66226da0ce1471
Zuleyma Pelcastre Vargas <pelcastrevargasz@gmail.com>
2025-03-20 18:43:31 -0600

    18 Implement authentication: Sing up

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -22,6 +22,29 @@ const apiService = {
                     reject(error)
                 }))
         })
+    },
+    post: async function (url: string, data: any): Promise<any> {
+        console.log ('post', url, data);
+
+        return new Promise ((resolve, reject) =>{
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`,{
+                method: 'POST',
+                body: data,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then((json) =>{
+                    console.log ('Respomse', json);
+
+                    resolve(json);
+            })
+            .catch((error => {
+                reject(error)
+            }))
+        })
     }
 }
 
